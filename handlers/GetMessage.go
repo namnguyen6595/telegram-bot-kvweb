cfbec3dcbc6b125f020cc8d3df3b6e4f8db047d9
nam.nt2 <nam.nt2@kiotviet.com>
2024-04-15 22:13:31 +0700

    handle message

--- handlers/GetMessage.go
+++ handlers/GetMessage.go
@@ -1,10 +1,13 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	qr_code "kvweb-bot/qr-code"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 type GetMessageHandler struct {
@@ -32,13 +35,12 @@ func (h *GetMessageHandler) NewServe(ctx *gin.Context) {
 		return
 	}
 
-	log.Printf("Receive message from data: %v", &Update{
-		UpdateID: update.UpdateID,
-		Message: Message{
-			Chat: update.Message.Chat,
-			Text: update.Message.Text,
-		},
-	})
+	messageData, err := json.MarshalIndent(update, "", "    ") // Sử dụng MarshalIndent để định dạng JSON cho dễ đọc
+	if err != nil {
+		log.Fatalf("Error occurred during marshaling. Error: %s", err.Error())
+	}
+
+	log.Printf("Receive message from data: %v", messageData)
 
 	// Kiểm tra xem tin nhắn có phải là lệnh /topup
 	if update.Message.Text == "/topup" {
@@ -46,6 +48,7 @@ func (h *GetMessageHandler) NewServe(ctx *gin.Context) {
 		qrCode, _ := qr_code.GenerateVietQrCode(&qr_code.GenerateQrRequest{
 			Amount: 200,
 		})
+		sendResponseToChat(update.Message.Chat.ID, qrCode.QrDataURL)
 		ctx.JSON(http.StatusOK, qrCode)
 		return
 	}
@@ -53,4 +56,22 @@ func (h *GetMessageHandler) NewServe(ctx *gin.Context) {
 	ctx.JSONP(http.StatusOK, map[string]string{
 		"data": "update",
 	})
+}
+
+func sendResponseToChat(chatId int64, imgUrl string) {
+	apiUrl := "https://api.telegram.org/bot6673474158:AAGWhE67vXABkSyL9H-ZCREhSzLrCfvDX48/setWebhook"
+	data := url.Values{}
+	data.Set("chat_id", fmt.Sprintf("%d", chatId))
+	data.Set("photo", imgUrl)
+
+	resp, err := http.PostForm(apiUrl, data)
+	if err != nil {
+		log.Fatalf("Error sending photo: %s", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Received non-OK response from Telegram: %s", resp.Status)
+	}
 }
